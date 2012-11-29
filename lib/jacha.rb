c33625372b08a4a8ff5700d948fa0803034ae72b
AlexanderPavlenko <a.pavlenko@roundlake.ru>
2012-11-29 15:40:47 +0400

    screw this

--- lib/jacha.rb
+++
@@ -1,9 +0,0 @@
-require 'jacha/version'
-require 'jacha/connection'
-require 'jacha/connection_pool'
-
-module Jacha
-  def self.configure
-    yield ConnectionPool.instance if block_given?
-  end
-end
--- lib/jacha/version.rb
+++
@@ -1,3 +0,0 @@
-module Jacha
-  VERSION = "0.0.2"
-end
